adee8141c55b0d620fd501ee92776644da6c7cd3
Cindy Tang <cindyt07@mit.edu>
2026-07-09 01:35:31 -0500

    Add: validate-parentheses - submission-6

---
+++ Data Structures & Algorithms/validate-parentheses/submission-6.py
@@ -0,0 +1,24 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        matchings = {
+            ")" : "(",
+            "}" : "{",
+            "]" : "["
+        }
+
+        queue = []
+        
+        for char in s:
+            if char in {"(", "{", "["}:
+                queue.append(char)
+            else:
+                # Correct Order
+                last_opened = queue.pop() if queue else False
+                if not last_opened or last_opened != matchings[char]:
+                    return False
+
+        # All closed
+        if queue:
+            return False
+
+        return True
